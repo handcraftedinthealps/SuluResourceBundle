3f55425190eebe7a6837dd4be11e07064999035d
Johannes Wachter <johannes@sulu.io>
2020-02-27 13:20:01 +0100

    added basic files

---
+++ HandcraftedInTheAlpsSuluResourceBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HandcraftedInTheAlps\Bundle\SuluResourceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HandcraftedInTheAlpsSuluResourceBundle extends Bundle
+{
+}
